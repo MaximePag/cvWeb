14cdc79413722259f56cca385321b79ffb20a299
MaximePag <max_p@live.fr>
2021-01-22 09:16:04 +0100

    debug des onglets

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,18 @@
 function toggle(tab, tabType){
     var allTabs = document.getElementsByClassName("tab");
     var allCards = document.getElementsByClassName("card");
-    for (i = 0; i < 6; i++){
+    for (i = 0; i < 5; i++){
         allTabs[i].classList.remove("tab-open");
+        allTabs[i].classList.remove("col-3");
         allTabs[i].classList.add("tab-close");
+        allTabs[i].classList.add("col-2");
         allCards[i].classList.remove("d-block");
         allCards[i].classList.add("d-none");
     }
     tab.classList.remove("tab-close");
     tab.classList.add("tab-open");
+    tab.classList.remove("col-2");
+    tab.classList.add("col-3");
     if (tabType == 'present'){
         var card = document.getElementById("present");
     }
